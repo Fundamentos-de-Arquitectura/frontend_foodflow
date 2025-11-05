905818c79aa9c6cdd660effa378e980eb3695a0a
Paolo <u20221f613@upc.edu.pe>
2025-11-05 17:19:21 -0500

    fix: adjust topbar and sidebar

--- src/app/core/layout/shell.component.ts
+++ src/app/core/layout/shell.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { TopbarComponent } from '../components/topbar/topbar.component';
 import { RouterOutlet } from '@angular/router';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [SidebarComponent, TopbarComponent, RouterOutlet, NgIf],
+  imports: [SidebarComponent, TopbarComponent, RouterOutlet],
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.css']
 })
